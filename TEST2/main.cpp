12c85bfc20cc26291ff4c20d4819b583784e282c
Andrew Zhdan <lagoon@bk.ru>
2023-05-01 23:15:52 +0500

    first commit

--- TEST2/main.cpp
+++ TEST2/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#
+#include <string>
+#include <vector>
+
+
 
 int main()
 {
-int size = 4; //1
-long mul = 1; //1
-for (int i = 1; i <= size; ++i) { // 2 + 2(4 раза) 
-  if(i % 2)  4 раза
-    mul *= i; 2 (2 раза
-    )
-std::cout << mul << std::endl;
+  
+std::vector <std::string> ivector{"Maria","Anna","Angela","Sasha","Nina","Galya"};
+
+for(int i = 0; i < sizeof(ivector);++i)
+{
+
+std::cout<< ivector[i] <<std::endl;
+}
+std::cout << "Add"<< std::endl;
+std::string i;
+std::cin >>  i;
+ivector.push_back(i);
+for(int i = 0; i < sizeof(ivector);++i)
+{
+
+std::cout<< ivector[i] <<std::endl;
 }
 
-int sum = 0; //1
-for (int i = 0; i < size; ++i) {  2+3     +3
-  sum += array[i];
-  sum = sum + array[i]   3
-}
+
+
+return 0;
 
 }
